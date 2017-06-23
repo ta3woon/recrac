3fcc2c9a3b5acd409dc6f395720fd78ffac5be38
ta3woon <ta3woon@gmail.com>
2017-06-23 18:34:36 -0400

    fixed create modal

--- index.js
+++ index.js
@@ -146,34 +146,40 @@ app.get('/timer', function(req, res) {
 // });
 
 //post method is a click event on Add event button
-app.post('/app/home', function(req, res){
-  var newEvent = new Event ({
-    name: req.body.name,
-    description: req.body.description,
-    host: req.body.host,
-    type: req.body.type,
-    time: req.body.time,
-    price: req.body.price || 0,
-    desiredParticipants: req.body.desiredParticipants,
-    //Need help inputing this may need a method to turn adress into coordinates
-    location: req.body.location 
-  });
-  newEvent.save(function(err, newEvent){
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.status(200).send(newEvent);
-    }
-  })
-});
+
+// app.post('/events', function(req, res){
+
+//   newEvent.save(function(err, newEvent){
+//     if (err) {
+//       res.status(500).send(err);
+//     } else {
+//       res.status(200).send(newEvent);
+//     }
+//   })
+// });
 
 app.post('/events', function(req, res) {
   const address = req.body.location;
+
   request(geocodeURL + address, function(err, response, body) {
+
+    console.log(body);
     req.body.location = JSON.parse(body).results[0].geometry.location
     req.body.location.address = address
 
-    var newEvent = new Event(req.body);
+    // var location = {address: req.body.location, req.location}
+
+    var newEvent = new Event ({
+      name: req.body.name,
+      description: req.body.description,
+      host: req.body.host,
+      type: req.body.type,
+      time: req.body.time,
+      price: req.body.price || 0,
+      desiredParticipants: req.body.desiredParticipants,
+      location: req.body.location
+    });
+
     newEvent.save(function(err, event) {
       if (err) {
         res.status(500).json(err);
@@ -183,7 +189,6 @@ app.post('/events', function(req, res) {
     })
   })
 });
-
 
 app.get('/events', function(req, res){
   Event.find({}, function(err, events) {
